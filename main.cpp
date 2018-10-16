671637f60f567bc53da76f3b5cb0556dcfb6ee0b
argraur <argraur@gmail.com>
2018-10-16 15:53:34 +0000

    Fix GUI code
    Signed-off-by: argraur <argraur@gmail.com>

--- main.cpp
+++ main.cpp
@@ -24,20 +24,13 @@ void InputToRect(Rectangle *A) {
 
 int main() {
 	Rectangle *A = new Rectangle;
-	Rectangle *B = new Rectangle;
 	InputToRect(A);
-	InputToRect(B);
-	std::cout << "In A" << std::endl;
-	A -> Out();
-	std::cout << "In B" << std::endl;
-	B -> Out();
 	for (;;) {
 		#ifdef linux
                 	system("/bin/bash -c clear");
         	#else
                 	system("CLS");
         	#endif
-		
 		std::cout << "1. Input dots" << std::endl;
 		std::cout << "2. Move"<< std::endl;
 		std::cout << "3. Resize"<< std::endl;
@@ -52,17 +45,13 @@ int main() {
         			#else
                 			system("CLS");
         			#endif
-							std::cout << "Exiting..."; 
+				std::cout << "Exiting...";
 				return 0;
 				break;
 			case 1:
 				std::cout << "Enter x then y of each dot one by one" << std::endl;
-				for ( int i = 0; i < 4; i++) {
-					int tmp, tmp1;
-					std::cin >> tmp >> tmp1;
-					A -> Input(i, tmp, tmp1);
-				}
-				std::cout << "Dots Added Successfully" << std::endl; 
+				InputToRect(A);
+				std::cout << "Dots Added Successfully" << std::endl;
 				#ifdef linux
                                         system("/bin/bash -c read");
                                 #else
@@ -70,33 +59,32 @@ int main() {
                                 #endif
 				break;
 			case 2:
-				int t, tt; 
+				int t, tt;
 				std::cout << "Enter how much do you want to move x: ";
-				std::cin >> t; 
+				std::cin >> t;
 				std::cout << "Enter how much do you want to move y: ";
-				std::cin >> tt; 
-
+				std::cin >> tt;
 				A -> Move(t, tt);
-                std::cout << "Moved Successfully" << std::endl; 
+		                std::cout << "Moved Successfully" << std::endl;
 				#ifdef linux
                                         system("/bin/bash -c read");
                                 #else
                                         system("pause");
                                 #endif
 				break;
-			case 3: 
-				float temp; 
+			case 3:
+				float temp;
 				std::cout << "Enter resize coefficient: ";
-				std::cin >> temp; 
+				std::cin >> temp;
 				A -> Resize(temp);
-				std::cout << "Resized Successfully"<< std::endl ; 
+				std::cout << "Resized Successfully"<< std::endl ;
 				#ifdef linux
                                         system("/bin/bash -c read");
                                 #else
                                         system("pause");
                                 #endif
 				break;
-			case 4:  
+			case 4:
 				A -> Out();
 				#ifdef linux
                                         system("/bin/bash -c read");
